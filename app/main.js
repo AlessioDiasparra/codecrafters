a2d1ca0b23e89486af16ac676a6ad70da130e106
Alessio Diasparra <alessiodiasparra@libero.it>
2024-09-09 00:51:40 +0200

    test extract the URL path from an HTTP request

--- app/main.js
+++ app/main.js
@@ -1,17 +1,36 @@
 const net = require("net");
-
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("HTTP/1.1 200 OK\r\n\r\n");
+console.log("HTTP/1.1 200 OK\\r\\n\\r\\n");
 
- // TODO: Uncomment this to pass the first stage
- const server = net.createServer((socket) => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n");
-   // Close the socket when the client closes the connection.
-   // This is necessary to prevent the server from hanging.
-   socket.on("close", () => {
-     socket.end();
-     server.close();
-   });
- });
+// TODO: Uncomment this to pass the first stage
+// Create an HTTP server that listens on port 4221 on localhost.
+// It will send a 200 OK response to each incoming request and close the connection.
+// This is a very basic server that does not handle any requests other than the initial connection.
+const server = net.createServer(socket => {
+  // When a new client connects, send a 200 OK response and close the connection.
+  socket.write("HTTP/1.1 200 OK\\r\\n\\r\\n");
+  var data = "";
+  socket.on("data", function (dataChunk) {
+    data += dataChunk;
+    socket.on("end", function () {
+      var arrayData = data.split("");
+      const requestedpath = arrayData[1];
+      const Requesturl = requestedpath.split(" ");
+      console.log(requestedpath);
+      console.log(server.address());
+      console.log(server.address().port);
+      console.log(requestedpath.startsWith("/unknown/"));
+      if (requestedpath.startsWith("/unknown/")) {
+        socket.write("HTTP/1.1 404 Not Found\\r\\n\\r\\n");
+      } else {
+        socket.write("HTTP/1.1 200 OK\\r\\n\\r\\n");
+      }
+    });
+    // When the client closes the connection, close the server.
 
- server.listen(4221, "localhost");
+    socket.on("close", () => {
+       //server.close();
+    });
+  });
+});
+server.listen(4221, "localhost");
