7bbacd5b6ac77596fd4ff43a769e8629a02a448a
Alessio Diasparra <alessiodiasparra@libero.it>
2024-09-09 00:27:28 +0200

    test server's response

--- app/main.js
+++ app/main.js
@@ -5,10 +5,12 @@ console.log("HTTP/1.1 200 OK\r\n\r\n");
 
  // TODO: Uncomment this to pass the first stage
  const server = net.createServer((socket) => {
+    socket.write("HTTP/1.1 200 OK\r\n\r\n");
    // Close the socket when the client closes the connection.
    // This is necessary to prevent the server from hanging.
    socket.on("close", () => {
      socket.end();
+     server.close();
    });
  });
 
