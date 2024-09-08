893ee2ed858bb347e4262374c0ffa7bae6f30df1
Alessio Diasparra <alessiodiasparra@libero.it>
2024-09-09 00:22:46 +0200

    test response server socket

--- app/main.js
+++ app/main.js
@@ -1,14 +1,14 @@
 const net = require("net");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("starting server");
+console.log("HTTP/1.1 200 OK\r\n\r\n");
 
  // TODO: Uncomment this to pass the first stage
  const server = net.createServer((socket) => {
    // Close the socket when the client closes the connection.
    // This is necessary to prevent the server from hanging.
    socket.on("close", () => {
-     socket.end("HTTP/1.1 200 OK\r\n\r\n");
+     socket.end();
    });
  });
 
